33398363020200e08d5bb6e47ec19ea4725a7d7d
Jakub <jakubowskijjal1@gmail.com>
2023-11-07 18:02:06 +0100

    set without repetitions :godmode:

--- src/brutalForce/set.java
+++ src/brutalForce/set.java
@@ -4,16 +4,17 @@ public class set
 {
     public static void main(String[] args)
     {
-        for(int a=1;a<=20; a++)
-            for(int b=1; b<=20; b++)
-                for(int c=1; c<=20; c++)
-                {
-                    if(a*a + b*b == c*c)
+        for(int a=0;a<=1; a++)
+            for(int b=0; b<=1; b++)
+                for(int c=0; c<=1; c++)
+                    for(int d=0; d<=1; d++)
                     {
-                        System.out.print(a+" "+b+" "+c+"\n");
+                        System.out.print("{ ");
+                        if(a==1) System.out.print("1 ");
+                        if(b==1) System.out.print("2 ");
+                        if(c==1) System.out.print("3 ");
+                        if(d==1) System.out.print("4 ");
+                        System.out.print("}\n");
                     }
-                }
     }
-
-    //program wyświetla trójki pitagorejskie
 }
