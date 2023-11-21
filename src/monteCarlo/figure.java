ce93d9bc1795c96f37dbe071370656f2edf70c31
Jakub <jakubowskijjal1@gmail.com>
2023-11-21 17:39:57 +0100

    nothing special

--- src/monteCarlo/figure.java
+++ src/monteCarlo/figure.java
@@ -4,22 +4,24 @@ import java.util.Random;
 
 public class figure
 {
+    static int draw = 100;
+
     public static void main(String[] args)
     {
         Random point = new Random();
         int couting = 0;
 
-        for(int i=0; i<100; i++)
+        for(int i=0; i<draw; i++)
         {
             double x = point.nextInt(100+1)/100.0;
             double y = point.nextInt(100+1)/100.0;
-//            System.out.println("x= "+x+", y= "+y);
+            System.out.println("x= "+x+", y= "+y);
 
             if(y<= Math.sin(x))
             {
                 couting++;
             }
         }
-        System.out.println("Pole = "+(couting/100.0));
+        System.out.println("Figure area = "+(couting/100.0));
     }
 }
