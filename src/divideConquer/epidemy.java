a99ede53085d3745d2736765a82d4376ab5a4793
Jakub <jakubowskijjal1@gmail.com>
2023-11-21 17:39:11 +0100

    epidemy 70% done

---
+++ src/divideConquer/epidemy.java
@@ -0,0 +1,30 @@
+package divideConquer;
+
+public class epidemy
+{
+    static int  population = 100000;
+    static final int  starting_sick_population = 10;
+    static final int starting_healthy_population = 100000;
+    static int days = 0;
+
+    public static void main(String[] args)
+    {
+        epidemy(starting_sick_population);
+    }
+
+    public static int epidemy(int sick)
+    {
+        days++;
+        population = starting_healthy_population - sick;
+        if(population <= 0) {
+            System.out.println("Whole population will get sick in "+days+" days");
+            return 0;
+        } else {
+            System.out.println("SICK: "+sick+ " HEALTHY: "+ population+ " DAYS: " +days);
+        }
+        sick = sick + sick*2;
+        return epidemy(sick);
+    }
+
+
+}
